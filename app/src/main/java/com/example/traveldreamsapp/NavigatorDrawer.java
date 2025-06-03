e16dcc0735f91916e08673e1aedcfb3d7d5cfd9c
Claudio Fernandez <cc.rr.ff.2014@gmail.com>
2025-06-02 22:17:50 -0300

    fix(mennú) corrige vinculo del icono carrito

--- app/src/main/java/com/example/traveldreamsapp/NavigatorDrawer.java
+++ app/src/main/java/com/example/traveldreamsapp/NavigatorDrawer.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,8 +15,8 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
-import androidx.core.text.HtmlCompat; // Importación necesaria para manejar HTML
-import androidx.appcompat.app.AlertDialog; // Importación para AlertDialog
+import androidx.core.text.HtmlCompat;
+import androidx.appcompat.app.AlertDialog;
 
 import com.example.traveldreamsapp.databinding.ActivityNavigatorDrawerBinding;
 
@@ -38,7 +39,8 @@ public class NavigatorDrawer extends AppCompatActivity implements NavigationView
         NavigationView navigationView = binding.navView;
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_destinos, R.id.nav_perfil, R.id.nav_contacto, R.id.nav_nosotros)
+                R.id.nav_home, R.id.nav_destinos, R.id.nav_perfil,
+                R.id.nav_contacto, R.id.nav_nosotros, R.id.nav_cart)
                 .setOpenableLayout(drawer)
                 .build();
 
@@ -55,7 +57,7 @@ public class NavigatorDrawer extends AppCompatActivity implements NavigationView
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         return NavigationUI.onNavDestinationSelected(item, navController)
                 || super.onOptionsItemSelected(item);
     }
@@ -68,25 +70,26 @@ public class NavigatorDrawer extends AppCompatActivity implements NavigationView
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
+        DrawerLayout drawer = binding.drawerLayout;
 
         if (id == R.id.nav_cart) {
-            startActivity(new Intent(this, DashboardCompras.class));
+            navController.navigate(R.id.nav_cart);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
         } else if (id == R.id.nav_help) {
             Snackbar.make(binding.getRoot(), "Clic en Ayuda desde Drawer", Snackbar.LENGTH_LONG)
                     .setAction("Cerrar", view -> {})
                     .show();
         } else if (id == R.id.nav_about) {
-            // Obtener la cadena HTML de strings.xml y formatearla
             String htmlString = getString(R.string.about_software_info);
             CharSequence formattedText = HtmlCompat.fromHtml(htmlString, HtmlCompat.FROM_HTML_MODE_LEGACY);
 
-            // Crear y mostrar un AlertDialog en lugar de un Snackbar
             new AlertDialog.Builder(this)
-                    .setTitle("Acerca de Travel Dreams App") // Título del diálogo
-                    .setMessage(formattedText) // Contenido con el texto formateado
-                    .setPositiveButton("Cerrar", (dialog, which) -> dialog.dismiss()) // Botón para cerrar el diálogo
+                    .setTitle("Acerca de Travel Dreams App")
+                    .setMessage(formattedText)
+                    .setPositiveButton("Cerrar", (dialog, which) -> dialog.dismiss())
                     .show();
         } else if (id == R.id.nav_privacy_policy) {
             startActivity(new Intent(this, PoliticaPrivacidad.class));
@@ -98,17 +101,14 @@ public class NavigatorDrawer extends AppCompatActivity implements NavigationView
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
             finish();
-        }
-        else {
+        } else {
             boolean handledByNavigation = NavigationUI.onNavDestinationSelected(item, navController);
             if (handledByNavigation) {
-                DrawerLayout drawer = binding.drawerLayout;
                 drawer.closeDrawer(GravityCompat.START);
                 return true;
             }
         }
 
-        DrawerLayout drawer = binding.drawerLayout;
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
